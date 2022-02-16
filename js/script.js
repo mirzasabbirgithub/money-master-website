20639d8fe32d5468e86cf19bb07d08634ece3926
mirzasabbirgithub <mirzaasabbir@gmail.com>
2022-02-17 02:21:40 +0600

    basic js complete without adding error messages

--- js/script.js
+++ js/script.js
@@ -24,11 +24,33 @@ document.getElementById('calculate-btn').addEventListener('click', function () {
     const totalIncome = income.value;
     const totalAmount = parseFloat(totalIncome);
 
+    //balance 
     const totalFinalBalance = totalAmount - totalCost;
 
-    //balance
+    //balance replaced
     const totalBalance = document.getElementById('last-balance');
     totalBalance.innerText = totalFinalBalance;
 
+    //savings
+    document.getElementById('save-btn').addEventListener('click', function () {
+        const savePercetage = document.getElementById('save-percentage');
+        const savePertageValue = savePercetage.value;
+        const savePercetageTotal = parseFloat(savePertageValue);
+        //calculate percentage
+        percetageTotal = (savePercetageTotal * totalFinalBalance) / 100;
 
+        //savings amount
+        const savingsTotal = document.getElementById('savings-amount');
+        savingsTotal.innerText = percetageTotal;
+
+        //remaining balance
+        remainingTotalBalance = totalFinalBalance - percetageTotal;
+
+        const remainingBalance = document.getElementById('remaining-balance');
+        remainingBalance.innerText = remainingTotalBalance;
+
+    });
 });
+
+
+
